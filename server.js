96a8e7573ac38b83b98d1ee54c66acf569fd9a40
Travis Brewer <t.brewer.co@gmail.com>
2021-06-15 20:11:03 -0600

    wip: configure mongo database

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 // dependencies
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Listener
+// listener
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`The server is listening on port: ${PORT}`));
+
+// database configuration
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+// database connection error/success notices
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + "mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
