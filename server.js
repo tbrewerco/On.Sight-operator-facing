d13d36506329b3ef80b2cc48fcdcf6e808348ed5
Travis Brewer <t.brewer.co@gmail.com>
2021-06-16 12:08:46 -0600

    wip: add body parser middleware

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// listener
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`The server is listening on port: ${PORT}`));
-
 // database configuration
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -21,3 +17,11 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + "mongo not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
+
+// middleware
+// body parser middleware
+app.use(express.urlencoded({ extended: false}));
+
+// listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`The server is listening on port: ${PORT}`));
