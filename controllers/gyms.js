ea18313d74f1a29aabac3cd5c50fbc5ff9c35409
Travis Brewer <t.brewer.co@gmail.com>
2021-06-16 18:27:28 -0600

    wip: refactor index controller to show gyms on index page

--- controllers/gyms.js
+++ controllers/gyms.js
@@ -3,15 +3,18 @@ const Gym = require("../models/gyms.js");
 const router = express.Router();
 const Author = require("../models/gyms.js");
 
-router.get("/", (req, res) => { // home route
-    res.render("gyms/index.ejs");
+router.get("/", (req, res) => { // define index controller
+    Gym.find({}, (err, foundGyms) => {
+        res.render("gyms/index.ejs")
+            gyms: foundGyms
+    });
 });
 
-router.get('/gyms/new', (req, res) => { // new route
+router.get('/gyms/new', (req, res) => { // define new controller
     res.render('gyms/new.ejs');
 });
 
-router.post("/", (req, res) => { // create route
+router.post("/", (req, res) => { // define create controller
     Gym.create(req.body, (err, createdGym) => {
         res.redirect("/gyms"); // redirect to gyms index
     });
